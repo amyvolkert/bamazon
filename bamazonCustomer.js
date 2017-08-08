c9c22898c06bb7de728d3aef007322798617713c
Amy Volkert <amyvolkert@comcast.net>
2017-08-08 17:41:02 -0600

    added connection query to update db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,3 @@
-// MORE item_id, product_name, price
-
 // Requiring npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
@@ -19,12 +17,12 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 	// Checking if connection is successful
-	else 
+	else
 		console.log("Welcome to BAMazon!");
 	start();
 })
 
-// Upon successful connection, comment out "Welcome..." and run query to display data
+// Prompting user to choose between two options - INQUIRE or EXIT
 var start = function(){
 	inquirer.prompt({
 		name: "inquireOrExit",
@@ -35,12 +33,17 @@ var start = function(){
 		if(answer.inquireOrExit.toUpperCase()=="INQUIRE") {
 			inquireProduct();
 		} else {
-			// exitBAMazon();
+			exitBAMazon();
 		}
 	})
 }
 
-//Running query to display available products
+// Running function if user chooses EXIT
+function exitBAMazon() {
+	console.log("Thank you. Come back soon!")
+}
+
+// Running query to display available products if user chooses INQUIRE
 var inquireProduct = function() {
 	connection.query("SELECT item_id, product_name, price FROM products", function(err,response){
 		console.log(response);
@@ -49,7 +52,7 @@ var inquireProduct = function() {
 				type: "rawlist",
 					choices: function(value){
 						var choiceArray = [];
-						for (var i=0; i<response.length; i++){
+						for (var i=0; i < response.length; i++){
 							choiceArray.push(response[i].product_name);
 						}
 						return choiceArray;
@@ -66,12 +69,13 @@ var inquireProduct = function() {
 							// validate:... ends
 							}
 			// inquirer.prompt ends
+			// Displaying users' choice and running inquireQuantity function
 			}).then(function(answer){
 					inquireQuantity();
 			// .then(function... ends
 			})
 	// connection.query ends
-	})
+	// }) ***
 		// Running quantity function inside of inquireProduct function
 		var products = ("SELECT * FROM products");
 		var inquireQuantity = function() {
@@ -79,7 +83,7 @@ var inquireProduct = function() {
 				name: "quantity",
 				type: "input",
 				message: "How many would you like to purchase?",
-					// Ensuring the user enters a number
+// TA			// Ensuring the user enters a number
 					// validate: function(value) {
 					// 	if (isNAN (value) === false) {
 					// 		return true;
@@ -90,17 +94,27 @@ var inquireProduct = function() {
 					// }
 			// inquirer.prompt ends
 			}).then(function(answer){
-				if(products.stock_quantity > parseInt(answer.quantity)){
-					console.log("The item(s) has been added to your cart.")
+				// Retrieveing data of the item selected
+				var selectedItem;
+					for (var i = 0; i < response.length; i++) {
+						if (response(i).item_id === answer.choice) {
+							selectedItem = response[i];
+						}
+					}
 
-			}
 			// .then(function... ends
 			})
-		// inquireQuantity function ends
-		}
-// inquireProduct function ends
-}
-
-
-
-	
+			// Determining if the item is in stock
+			if (selectedItem.stock_quantity > parseInt(answer.quantity)) {
+				// Item is in stock
+				connection.query(
+					"UPDATE products SET stock_quantity = stock_quantity MINUS answer.quantity WHERE item_id = answer.id"
+				)
+				console.log("Your order is complete!")
+					else {
+						console.log("We're sorry, the product you've requested has " + stock_quantity + "available.")
+						inquireProduct();
+					}
+				}
+			)
+		})
